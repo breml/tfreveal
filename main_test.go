4e17ba9598093d86951f8145e370199abb53668d
Lucas Bremgartner <lucas@bremis.ch>
2024-05-24 14:48:52 +0200

    Add tests

---
+++ main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain0(t *testing.T) {
+	files, err := filepath.Glob(filepath.Join("testdata", "*"))
+	require.NoError(t, err)
+
+	for _, filename := range files {
+		filename := filename
+		t.Run(filename, func(t *testing.T) {
+			defer func(orig *os.File) {
+				os.Stdout = orig
+			}(os.Stdout)
+
+			r, w, err := os.Pipe()
+			require.NoError(t, err)
+
+			os.Stdout = w
+
+			err = main0([]string{"tfreveal", "--no-color", filepath.Join(filename, "plan.json")})
+			require.NoError(t, err)
+			err = w.Close()
+			require.NoError(t, err)
+
+			out, err := io.ReadAll(r)
+			require.NoError(t, err)
+
+			want, err := os.ReadFile(filepath.Join(filename, "output.golden"))
+			require.NoError(t, err)
+
+			require.Equal(t, string(want), string(out))
+		})
+	}
+}
+
+func captureStdout(f func() error) string {
+	// return to original state afterwards
+	// note: defer evaluates (and saves) function ARGUMENT values at definition
+	// time, so the original value of os.Stdout is preserved before it is
+	// changed further into this function.
+	defer func(orig *os.File) {
+		os.Stdout = orig
+	}(os.Stdout)
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	f()
+	w.Close()
+	out, _ := io.ReadAll(r)
+
+	return string(out)
+}
