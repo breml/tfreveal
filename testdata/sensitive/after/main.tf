25a0ebb8588f651929246961a2740d5ff0cce234
Lucas Bremgartner <lucas@bremis.ch>
2024-05-26 13:40:17 +0200

    Add more test cases with sensitive values in JSON

--- testdata/sensitive/after/main.tf
+++ testdata/sensitive/after/main.tf
@@ -1,5 +1,9 @@
 locals {
-  secret_value = sensitive("new secret value")
+  secret_value   = sensitive("new secret value")
+  sensitive_json = sensitive(jsonencode({ key = "value2" }))
+  json_with_nested_sensitive = jsonencode({
+    key = sensitive("value2")
+  })
   other_value = jsonencode(
     {
       key = "value"
@@ -13,6 +17,23 @@ resource "null_resource" "sensitive" {
   }
 }
 
+output "null_resource" {
+  value     = nonsensitive(null_resource.sensitive)
+  sensitive = true
+}
+
 output "other_value" {
   value = local.other_value
+}
+
+output "sensitive" {
+  value = nonsensitive(null_resource.sensitive.triggers.secret)
+}
+
+output "sensitive_json" {
+  value = nonsensitive(local.sensitive_json)
+}
+
+output "json_with_nested_sensitive" {
+  value = nonsensitive(local.json_with_nested_sensitive)
 }
--- testdata/sensitive/before/main.tf
+++ testdata/sensitive/before/main.tf
@@ -1,9 +1,30 @@
 locals {
-  secret_value = sensitive("some secret value")
+  secret_value   = sensitive("some secret value")
+  sensitive_json = sensitive(jsonencode({ key = "value" }))
+  json_with_nested_sensitive = jsonencode({
+    key = sensitive("value")
+  })
 }
 
 resource "null_resource" "sensitive" {
   triggers = {
     secret = local.secret_value
   }
+}
+
+output "null_resource" {
+  value     = nonsensitive(null_resource.sensitive)
+  sensitive = true
+}
+
+output "sensitive" {
+  value = nonsensitive(null_resource.sensitive.triggers.secret)
+}
+
+output "sensitive_json" {
+  value = nonsensitive(local.sensitive_json)
+}
+
+output "json_with_nested_sensitive" {
+  value = nonsensitive(local.json_with_nested_sensitive)
 }
